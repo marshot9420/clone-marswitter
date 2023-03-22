5368025f405bf0801a88ebe9e5e5c1291d321eb9
marshot9420 <marshot9420@gmail.com>
2023-03-22 13:13:46 +0900

    Firebase Set Up

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBSCAAsiRHmMRojIwtP8F7p7b7qoW-F48k",
+  authDomain: "marstwit-acd17.firebaseapp.com",
+  projectId: "marstwit-acd17",
+  storageBucket: "marstwit-acd17.appspot.com",
+  messagingSenderId: "32239672425",
+  appId: "1:32239672425:web:7b541c42f64a5b224796f8",
+};
+
+export default firebase.initializeApp(firebaseConfig);
