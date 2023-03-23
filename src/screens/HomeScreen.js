0202af7d13c701479a6ae45babcb3e9dd38400e8
marshot9420 <marshot9420@gmail.com>
2023-03-23 12:48:28 +0900

    Feature / Getting the MarsWits

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,28 @@
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, doc, getDocs, query } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { dbService } from "../fbase";
 
 const HomeScreen = () => {
   const [marswit, setMarswit] = useState("");
+  const [marswits, setMarswits] = useState([]);
+
+  const getMarswits = async () => {
+    const querySnapshot = await getDocs(
+      query(collection(dbService, "marswits"))
+    );
+    querySnapshot.forEach((doc) => {
+      const marswitObject = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      setMarswits((prev) => [marswitObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getMarswits();
+  }, []);
+
   const onSubmit = async (event) => {
     event.preventDefault();
     const docRef = await addDoc(collection(dbService, "marswits"), {
@@ -13,12 +32,14 @@ const HomeScreen = () => {
     console.log(docRef);
     setMarswit("");
   };
+
   const onChange = (event) => {
     const {
       target: { value },
     } = event;
     setMarswit(value);
   };
+
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -31,6 +52,13 @@ const HomeScreen = () => {
         />
         <input type="submit" value="MarsWit" />
       </form>
+      <div>
+        {marswits.map((marswit) => (
+          <div key={marswit.id}>
+            <h4>{marswit.marswit}</h4>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
