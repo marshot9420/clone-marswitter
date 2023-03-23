4c7ca28e5ead9d9152b96ba89f63c4cee7572e94
marshot9420 <marshot9420@gmail.com>
2023-03-23 11:22:50 +0900

    Feature / Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import AuthScreen from "../screens/AuthScreen";
 import HomeScreen from "../screens/HomeScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import Navigation from "./Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/profile" replace to="/" element={<ProfileScreen />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </>
         ) : (
           <>
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { authService } from "../fbase";
 
 const ProfileScreen = () => {
-  return <span>ProfileScreen</span>;
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
 };
 
 export default ProfileScreen;
